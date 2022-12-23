c5f69caa5a83fd7984dd8e0fe32ac383f823fe72
allankevinscain <meuemail44allan@gmail.com>
2022-12-23 10:26:26 -0300

    fix: components

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,9 @@
 module.exports = {
   extends: '@oracle-cx-commerce/babel-config',
   plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    ['@babel/plugin-proposal-private-methods', { loose: false }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
     [
       'module-resolver',
       {
